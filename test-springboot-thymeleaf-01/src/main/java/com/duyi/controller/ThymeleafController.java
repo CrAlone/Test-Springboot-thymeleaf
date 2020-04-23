e9c092481a42d14dcc5153a14477a1c0b5e14567
${CrAlone} <${Cr__Alone@163.com}>
2020-04-24 01:37:19 +0800

    尝试第二次增改

--- test-springboot-thymeleaf-01/src/main/java/com/duyi/controller/ThymeleafController.java
+++ test-springboot-thymeleaf-01/src/main/java/com/duyi/controller/ThymeleafController.java
@@ -1,5 +1,6 @@
 package com.duyi.controller;
 
+import com.duyi.domian.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,27 @@ public class ThymeleafController {
      * 变量表达式
      */
     @RequestMapping("/tmf")
-    @ResponseBody
     public String demo(Model model){
-        model.addAttribute("name","陈大大");
-        return "/thymeleaf/index.html";
+        model.addAttribute("name","<p>陈大大</p>");
+        return "/thymeleaf/index";
+    }
+    /**
+     * 选择变量表达式
+     */
+    @RequestMapping("/person")
+    public String demo2(Model model){
+        Person p = new Person();
+        p.setName("陈大大");
+        p.setAge(18);
+        p.setSex("男");
+        model.addAttribute("p",p);
+        return "/thymeleaf/person";
+    }
+    /**
+     * url表达式
+     */
+    @RequestMapping("/welcome")
+    public String demo3(){
+        return "/thymeleaf/url";
     }
 }
---
+++ test-springboot-thymeleaf-01/src/main/java/com/duyi/domian/Person.java
@@ -0,0 +1,9 @@
+package com.duyi.domian;
+
+import lombok.Data;
+@Data
+public class Person {
+    private String name;
+    private Integer age;
+    private String sex;
+}
